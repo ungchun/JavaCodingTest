3d945de48a82fadfc653e32c817b1b8c557e911f
sunghun <leedool3003@gmail.com>
2023-04-04 10:35:37 +0900

    [Programmers] 컨트롤 제트

---
+++ src/Programmers/컨트롤_제트.java
@@ -0,0 +1,19 @@
+package Programmers;
+class Solution_컨트롤_제트 {
+    public int solution(String s) {
+        int answer = 0;
+        int beforeValue = 0;
+        String[] Arrays = s.split(" ");
+
+        for(int i = 0; i < Arrays.length; i++) {
+            if (Arrays[i].equals("Z")) {
+                answer -= beforeValue;
+            } else {
+                answer += Integer.parseInt(Arrays[i]);
+                beforeValue = Integer.parseInt(Arrays[i]);
+            }
+        }
+
+        return answer;
+    }
+}
