7ec6cd4b7a9fe810627e2602375857f03e70b186
sunghun <leedool3003@gmail.com>
2023-04-07 09:07:39 +0900

    [Programmers] 삼각형의 완성조건 (2)

---
+++ src/Programmers/삼각형의_완성조건_2.java
@@ -0,0 +1,26 @@
+package Programmers;
+
+import java.util.*;
+
+class Solution_삼각형의_완성조건_2 {
+    public int solution(int[] sides) {
+        int answer = 0;
+
+        int max = Arrays.stream(sides).max().getAsInt();
+        int beforeAddValue = sides[0] > sides[1] ? sides[1] : sides[0];
+        beforeAddValue = max - beforeAddValue + 1;
+        while (beforeAddValue != max) {
+            answer += 1;
+            beforeAddValue += 1;
+        }
+
+        beforeAddValue = sides[0] + sides[1];
+
+        while (max != beforeAddValue) {
+            answer += 1;
+            max += 1;
+        }
+
+        return answer;
+    }
+}
