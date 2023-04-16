46550a0ba46b954b90c10aa3ae5ff86bffb3e19d
sunghun <leedool3003@gmail.com>
2023-04-16 19:49:33 +0900

    [Programmers] 치킨 쿠폰

---
+++ src/Programmers/치킨_쿠폰.java
@@ -0,0 +1,17 @@
+package Programmers;
+class Solution_치킨_쿠폰 {
+    final int BONUS_NUM = 10;
+    public int solution(int chicken) {
+        int answer = 0;
+
+        while (chicken >= BONUS_NUM) {
+            int newChick = chicken / BONUS_NUM;
+            int restChick = chicken % BONUS_NUM;
+            chicken = newChick + restChick;
+
+            answer += newChick;
+        }
+
+        return answer;
+    }
+}
