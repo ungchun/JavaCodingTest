d13574f7b219146455f73eb68ec74001c99785fc
sunghun <leedool3003@gmail.com>
2023-04-25 09:25:10 +0900

    [Programmers] 문자열 뒤의 n글자

---
+++ src/Programmers/문자열_뒤의_n글자.java
@@ -0,0 +1,7 @@
+package Programmers;
+
+class Solution_문자열의_뒤의_n글자 {
+    public String solution(String my_string, int n) {
+        return my_string.substring( my_string.length()-n, my_string.length());
+    }
+}
