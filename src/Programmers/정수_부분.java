541dd26a9becc374a4511ea408ec6ac94e4d647d
sunghun <leedool3003@gmail.com>
2023-04-28 09:20:38 +0900

    [Programmers] 정수 부분

---
+++ src/Programmers/정수_부분.java
@@ -0,0 +1,11 @@
+package Programmers;
+
+class Solution_정수_부분 {
+    public int solution(double flo) {
+        int answer = 0;
+
+        answer=(int)Math.floor(flo);
+
+        return answer;
+    }
+}
