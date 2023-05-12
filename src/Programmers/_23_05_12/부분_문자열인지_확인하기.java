ae4bb5a2079fd2984e7bddc6b8f4485d07bdadcf
sunghun <leedool3003@gmail.com>
2023-05-12 13:43:12 +0900

    [Programmers] 부분 문자열인지 확인하기

---
+++ src/Programmers/_23_05_12/부분_문자열인지_확인하기.java
@@ -0,0 +1,7 @@
+package Programmers._23_05_12;
+
+class Solution_부분_문자열인지_확인하기 {
+    public int solution(String my_string, String target) {
+        return my_string.contains(target) ? 1 : 0;
+    }
+}
