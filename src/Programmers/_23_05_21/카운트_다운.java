6f04f03d166d1ccc7eaaa36f5e907fc8a1142f5d
sunghun <leedool3003@gmail.com>
2023-05-21 23:07:43 +0900

    [Programmers] 카운트 다운

---
+++ src/Programmers/_23_05_21/카운트_다운.java
@@ -0,0 +1,15 @@
+package Programmers._23_05_21;
+
+class Solution_카운트_다운 {
+    public int[] solution(int start, int end) {
+        int[] answer = new int[start - end + 1];
+        int count = 0;
+
+        for (int i = start; i >= end; i--) {
+            answer[count] = i;
+            count++;
+        }
+
+        return answer;
+    }
+}
