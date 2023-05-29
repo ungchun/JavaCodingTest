feaf3a203ada6db803fe140fcaddfa35f73deb6c
sunghun <leedool3003@gmail.com>
2023-05-29 16:22:37 +0900

    [Programmers] 약수의 합

---
+++ src/Programmers/_23_05_29/약수의_합.java
@@ -0,0 +1,13 @@
+package Programmers._23_05_29;
+
+class Solution_약수의_합 {
+    public int solution(int n) {
+        int answer = 0;
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                answer += i;
+            }
+        }
+        return answer;
+    }
+}
