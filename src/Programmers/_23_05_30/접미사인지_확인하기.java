1899a063cf9ffb4362dbba59a13a6bd328ec5156
sunghun <leedool3003@gmail.com>
2023-05-30 11:05:03 +0900

    [Programmers] 접미사인지 확인하기

---
+++ src/Programmers/_23_05_30/접미사인지_확인하기.java
@@ -0,0 +1,7 @@
+package Programmers._23_05_30;
+
+class Solution_접미사인지_확인하기 {
+    public int solution(String my_string, String is_suffix) {
+        return my_string.endsWith(is_suffix) ? 1 : 0;
+    }
+}
