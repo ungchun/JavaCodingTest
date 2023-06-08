4f6d8e8127f8fa789dfcda46cb0b251da5246ace
sunghun <leedool3003@gmail.com>
2023-06-08 10:11:37 +0900

    [Programmers] l로 만들기

---
+++ src/Programmers/_23_06_08/l로_만들기.java
@@ -0,0 +1,13 @@
+package Programmers._23_06_08;
+
+class Solution_l로_만들기 {
+    public String solution(String myString) {
+        StringBuilder answer = new StringBuilder(myString);
+        for (int i =0 ; i < myString.length(); i++) {
+            if (answer.toString().charAt(i) < 'l') {
+                answer.setCharAt(i, 'l');
+            }
+        }
+        return answer.toString();
+    }
+}
