28150948b91616c55ed41fa19a29452f86170f13
sunghun <leedool3003@gmail.com>
2023-06-12 22:54:00 +0900

    [Programmers] 덧셈식 출력하기

---
+++ src/Programmers/_23_06_12/덧셈식_출력하기.java
@@ -0,0 +1,14 @@
+package Programmers._23_06_12;
+
+import java.util.Scanner;
+
+class Solution_덧셈식_출력하기 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+
+        System.out.println(String.valueOf(a) + " + " + String.valueOf(b) + " = " + (a+b) );
+    }
+}
